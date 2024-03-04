219bf9a2f1399a23fb5d775e7ca14fa6dd5767f5
LimpSword <contact@limpsword.fr>
2024-03-04 21:58:32 +0100

    Go wrapper to run assembly code

---
+++ asm/executor.go
@@ -0,0 +1,48 @@
+package asm
+
+import (
+	"github.com/charmbracelet/log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	start = "---- PROGRAM OUTPUT ----"
+	end   = "---- END PROGRAM OUTPUT ----"
+)
+
+func Execute(relativePath string) []string {
+	file, err := os.Open(relativePath)
+	if err != nil {
+		log.Fatal("Error while opening file")
+	}
+	defer file.Close()
+	absolutePath, err := filepath.Abs(relativePath)
+	cmd := exec.Command("java", "-jar", "asm/asm.jar", absolutePath)
+
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal("Error while executing file")
+	}
+	text := string(out)
+	lines := strings.Split(text, "\n")
+
+	programOutput := make([]string, 0)
+
+	prnt := false
+	for _, line := range lines {
+		if strings.Contains(line, end) {
+			prnt = false
+		}
+		if prnt {
+			programOutput = append(programOutput, line)
+		}
+		if strings.Contains(line, start) {
+			prnt = true
+		}
+	}
+
+	return programOutput
+}
